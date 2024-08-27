64f38aa9abda59d6c7b03947ad9afbe7650543f8
Albin Forsberg <96afo02@gmail.com>
2024-08-27 18:38:09 +0200

    Refactor and enhance topic and card rendering functionality.
    - Consolidated topic and card routes by introducing a dynamic route `/<string>/card` to handle card requests based on the selected topic. This replaces the previous static routes for specific topics.
    - Added logic to handle URL-encoded spaces in topic names, ensuring correct topic identification and card retrieval.
    - Modified `main.cpp` to improve content generation by initializing the `content` variable with an `<h1>Topics</h1>` header.
    - Updated templates:
      - Removed redundant "Next" button from `index.html`.
      - Added a "Next" button to `card.html` to load new cards dynamically based on the current topic.
      - Enhanced `topic.html` to include dynamic loading of cards upon clicking a topic.

--- src/main.cpp
+++ src/main.cpp
@@ -24,33 +24,29 @@ int main() {
     CROW_ROUTE(app, "/")([&cs](){
         set<string> topics = cs.get_all_topics();
         crow::mustache::context topic_ctx;
-        string content;
+
+        string content = "<h1>Topics</h1>";
         for (const string& t : topics)
         {
             crow::mustache::context ctx;
             ctx["topic"] = t;
             content += crow::mustache::load("topic.html").render_string(ctx);
         }
+
         crow::mustache::context base_ctx;
         base_ctx["content"] = content;
         return crow::mustache::load("index.html").render(base_ctx);
     });
 
-    CROW_ROUTE(app, "/science")([&cs](){
-        auto cards = cs.filter_cards_on_topic("Science");
-        auto card = cs.get_random_card(cards);
+    CROW_ROUTE(app, "/<string>/card")([&cs](string topic){
+        if (topic.find("%20") != string::npos)
+        {
+            topic.replace(topic.find("%20"), 3, " ");
+        }
+        cout << topic << endl;
+        auto card = cs.get_random_card(cs.filter_cards_on_topic(topic));
         auto content_ctx = load_card_context(card);
-        auto content = crow::mustache::load("card.html").render_string(content_ctx);
-        crow::mustache::context base_ctx;
-        base_ctx["content"] = content;
-        return crow::mustache::load("index.html").render(base_ctx);
-    });
-
-    CROW_ROUTE(app, "/card")([&cs](){
-        auto card = cs.get_random_card(cs.get_cards());
-        auto content_ctx = load_card_context(card);
-        auto content = crow::mustache::load("card.html").render(content_ctx);
-        return content;
+        return crow::mustache::load("card.html").render(content_ctx);
     });
 
     app.port(18080).multithreaded().run();
